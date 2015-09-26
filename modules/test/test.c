cfc1b79ad038f693e48937752847bd946ac7db33
Josh Rahm <joshuarahm@gmail.com>
2015-09-26 15:31:00 -0600

    Test module added

---
+++ modules/test/test.c
@@ -0,0 +1,13 @@
+#include "plugin.h"
+#include <stdio.h>
+
+AIRSTAT_PLUGIN(CONSUMER, "TestPlugin")
+
+void print_address(void* p)
+{
+    printf("%p\n", p);
+}
+
+AIRSTAT_EXPORT_BEGIN()
+AIRSTAT_EXPORT(print_address)
+AIRSTAT_EXPORT_END()
